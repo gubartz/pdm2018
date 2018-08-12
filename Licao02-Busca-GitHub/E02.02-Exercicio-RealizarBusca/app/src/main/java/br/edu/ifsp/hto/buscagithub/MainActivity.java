73687ff4711db9d10c5a70ecf7c8e8291b52adf7
Gustavo Bartz Guedes <gubartz@gmail.com>
2018-08-12 00:15:41 -0300

    Removendo a resolução do E02-02-Exercicio

--- Licao02-Busca-GitHub/E02.02-Exercicio-RealizarBusca/app/src/main/java/br/edu/ifsp/hto/buscagithub/MainActivity.java
+++ Licao02-Busca-GitHub/E02.02-Exercicio-RealizarBusca/app/src/main/java/br/edu/ifsp/hto/buscagithub/MainActivity.java
@@ -29,35 +29,15 @@ public class MainActivity extends AppCompatActivity {
 
     //TODO(3) Definir um método onClickBtProcurar(View view).
         // Esse método será executado quando o usuário clicar no botão Procurar.
-    public void onClickBtProcurar(View view){
         //TODO(4) Dentro do método onClickBtProcurar
-        //TODO(5) Recuperar o texto digitado pelo usuário no EditText
-            //Verificar se o texto não está vazio. Utilize o TextUtils.isEmpty
-            //Se o texto estiver vazio utilize return para encerrar o método
-        String busca = mBuscaEditText.getText().toString();
-
-        if(TextUtils.isEmpty(busca)){
-            return;
-        }
-
-        //TODO(6) Instanciar um objeto RestClient
-        RestClient restClient = new RestClient();
-
-        //TODO(7) Chamar o método getGitHubService.
-            // Atribuir seu retorno para uma variável correspondente.
-        GitHubService gitHubService = restClient.getGitHubService();
-
-        //TODO(8) Chamaro método listRepositories na variável recuperada no passo anterior.
-            // Atribuir seu retorno para uma variável correspondente
-        Call<ResponseBody> responseBodyCall = gitHubService.listRepositories(busca);
-        //TODO(9) Chamar o método execute na variável recuperada no passo anterior.
-        try {
-            responseBodyCall.execute();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        //TODO(10) Executar o programa e identificar o erro que ocorre.
-    }
-
-
+            //TODO(5) Recuperar o texto digitado pelo usuário no EditText
+                //Verificar se o texto não está vazio. Utilize o TextUtils.isEmpty
+                //Se o texto estiver vazio utilize return para encerrar o método
+                //TODO(6) Instanciar um objeto RestClient
+                //TODO(7) Chamar o método getGitHubService.
+                    // Atribuir seu retorno para uma variável correspondente.
+                //TODO(8) Chamaro método listRepositories na variável recuperada no passo anterior.
+                    // Atribuir seu retorno para uma variável correspondente
+                //TODO(9) Chamar o método execute na variável recuperada no passo anterior.
+                //TODO(10) Executar o programa e identificar o erro que ocorre.
 }
