5aa765058b1f97623b92ab204912684f16e027d9
Gustavo Bartz Guedes <gubartz@gmail.com>
2018-08-17 17:58:34 -0300

    Adicionando E03.05-Solucao

---
+++ Licao03-Webservice/E03.05-Solucao-Mensagem/android/app/src/main/java/br/edu/ifsp/hto/listardados/entities/Resource.java
@@ -0,0 +1,17 @@
+package br.edu.ifsp.hto.listardados.entities;
+
+import android.support.annotation.Nullable;
+
+//TODO(1) Colocar um generic T na declaração da classe
+public class Resource<T> {
+    //TODO(2) Definir um atributo final do tipo generic T chamado data
+    @Nullable public final T data;
+    //TODO(3) Definir um atributo final do tipo String chamado message
+    @Nullable public final String message;
+
+    //TODO(4) Definir um construtor com os atributos de classe
+    public Resource(T data, String message) {
+        this.data = data;
+        this.message = message;
+    }
+}
---
+++ Licao03-Webservice/E03.05-Solucao-Mensagem/webservice/src/settings.php
@@ -0,0 +1,29 @@
+<?php
+return [
+    'settings' => [
+        'displayErrorDetails' => true, // set to false in production
+        'addContentLengthHeader' => false, // Allow the web server to send the content-length header
+
+        // Renderer settings
+        'renderer' => [
+            'template_path' => __DIR__ . '/../templates/',
+        ],
+
+        // Monolog settings
+        'logger' => [
+            'name' => 'slim-app',
+            'path' => isset($_ENV['docker']) ? 'php://stdout' : __DIR__ . '/../logs/app.log',
+            'level' => \Monolog\Logger::DEBUG,
+        ],
+        
+        //Todo(1) Verificar se os dados para conexão ao BD estão corretos.
+        //database
+        'db' => [
+            'driver' => 'mysql',
+            'host'   => 'localhost',
+            'dbname' => 'lista_espera',
+            'user'   => 'root',
+            'pass'   => 'root'
+        ]
+    ],
+];
