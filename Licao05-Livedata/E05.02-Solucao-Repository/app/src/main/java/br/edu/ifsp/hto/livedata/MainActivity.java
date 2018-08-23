54ac7e7143f282b941636ecd0fc785cb460fecb2
Gustavo Bartz Guedes <gubartz@gmail.com>
2018-08-23 16:35:07 -0300

    Adicionando E05.02-Solucao-Repository

---
+++ Licao05-Livedata/E05.02-Solucao-Repository/app/src/main/java/br/edu/ifsp/hto/livedata/MainActivity.java
@@ -0,0 +1,65 @@
+package br.edu.ifsp.hto.livedata;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.List;
+
+import br.edu.ifsp.hto.livedata.adapter.ListaEsperaAdapter;
+import br.edu.ifsp.hto.livedata.database.AppDatabase;
+import br.edu.ifsp.hto.livedata.entities.ListaEspera;
+import br.edu.ifsp.hto.livedata.repositories.ListaEsperaRepository;
+import br.edu.ifsp.hto.livedata.utilities.AppExecutors;
+
+public class MainActivity extends AppCompatActivity {
+    private AppDatabase mDb;
+    private ListaEsperaAdapter mListaEsperaAdapter;
+    private RecyclerView mListaEsperaRecyclerView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mListaEsperaAdapter = new ListaEsperaAdapter();
+        mListaEsperaRecyclerView = findViewById(R.id.rv_lista_espera);
+        mListaEsperaRecyclerView.setLayoutManager(new LinearLayoutManager(this));
+        mListaEsperaRecyclerView.setAdapter(mListaEsperaAdapter);
+
+        mDb = AppDatabase.getsInstance(getApplicationContext());
+
+        loadInitialData();
+
+        carregarListaEspera();
+    }
+
+    private void carregarListaEspera(){
+        //TODO(6) Trocar o código abaixo para utilizar o ListaEsperaRepository
+            //LiveData<List<ListaEspera>> listLiveData = mDb.listaEsperaDAO().loadAllListaEspera();
+        LiveData<List<ListaEspera>> listLiveData = new ListaEsperaRepository(getApplication()).getAllListaEspera();
+
+        listLiveData.observe(this, new Observer<List<ListaEspera>>() {
+            @Override
+            public void onChanged(@Nullable List<ListaEspera> listaEsperas) {
+                mListaEsperaAdapter.setListaEspera(listaEsperas);
+            }
+        });
+    }
+
+    private void loadInitialData(){
+        AppExecutors.getInstance().diskIO().execute(new Runnable() {
+            @Override
+            public void run() {
+                mDb.listaEsperaDAO().clearTable();
+                mDb.listaEsperaDAO().insertListaEspera(ListaEspera.populateData());
+            }
+        });
+    }
+
+}
---
+++ Licao05-Livedata/E05.02-Solucao-Repository/app/src/main/java/br/edu/ifsp/hto/livedata/repositories/ListaEsperaRepository.java
@@ -0,0 +1,28 @@
+package br.edu.ifsp.hto.livedata.repositories;
+
+import android.app.Application;
+import android.arch.lifecycle.LiveData;
+
+import java.util.List;
+
+import br.edu.ifsp.hto.livedata.database.AppDatabase;
+import br.edu.ifsp.hto.livedata.entities.ListaEspera;
+
+public class ListaEsperaRepository {
+    //TODO(1) Declarar um atributo do tipo AppDatabase
+    private AppDatabase mDb;
+
+    //TODO(2) Declarar um construtor que recebe um Application
+        //Dentro do construtor
+    public ListaEsperaRepository(Application application) {
+        //TODO(3) Inicializar o atributo do passo 1 recuperando uma instância de AppDatabase
+        mDb = AppDatabase.getsInstance(application);
+    }
+
+    //TODO(4) Definir um metódo getAllListaEspera que retorna um LiveData de uma lista de objetos ListaEspera
+        //Dentro de getAllListaEspera
+    public LiveData<List<ListaEspera>> getAllListaEspera(){
+        //TODO(5) Retonar o resultado da busca utilizando o DAO
+        return mDb.listaEsperaDAO().loadAllListaEspera();
+    }
+}
