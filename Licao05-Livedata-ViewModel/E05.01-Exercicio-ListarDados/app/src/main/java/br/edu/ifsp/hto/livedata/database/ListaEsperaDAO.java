3f8b542f02d7a0a676e23a7dab0d2a6452710707
Gustavo Bartz Guedes <gubartz@gmail.com>
2018-09-28 22:03:36 -0300

    Removendo códigos dos todos

--- Licao05-Livedata-ViewModel/E05.01-Exercicio-ListarDados/app/src/main/java/br/edu/ifsp/hto/livedata/database/ListaEsperaDAO.java
+++ Licao05-Livedata-ViewModel/E05.01-Exercicio-ListarDados/app/src/main/java/br/edu/ifsp/hto/livedata/database/ListaEsperaDAO.java
@@ -19,23 +19,15 @@ public interface ListaEsperaDAO {
     //TODO(9) Definir um método chamado loadAllListaEspera, que retorna uma LiveData de lista de ListaEspera
     //TODO(10) Utilizar a annotation query e passar a consulta SQL para listar todos os dados da tabela lista_espera.
         //Ordenar pela data de cadastro
-    @Query("SELECT * FROM lista_espera ORDER BY data_cadastro")
-    LiveData<List<ListaEspera>> loadAllListaEspera();
 
     //TODO(11) Definir um método chamado insertListaEspera, que recebe um ListaEspera
     //TODO(12) Utilizar a annotation Insert
-    @Insert
-    void insertListaEspera(ListaEspera listaEspera);
 
     //TODO(13) Definir um método chamado updateListaEspera que recebe um ListaEspera
     //TODO(14) Utilizar a annotation Update com onConflict = OnConflictStrategy.REPLACE
-    @Update(onConflict = OnConflictStrategy.REPLACE)
-    void updateListaEspera(ListaEspera listaEspera);
 
     //TODO(15) Definir um método chamado deleteListaEspera que recebe um ListaEspera
     //TODO(16) Utilizar a annotation Delete
-    @Delete
-    void deleteListaEspera(ListaEspera listaEspera);
 
     @Insert
     void insertListaEspera(List<ListaEspera> listaEsperas);
